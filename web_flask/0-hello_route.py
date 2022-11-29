2258eae5133e883255544b939808f693cfd7f3fb
Damian <damiancaetano25@gmail.com>
2022-11-29 18:37:33 +0000

    Add comments and fix pycodestyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+""" A script that starts Flask web application
+
+    Return:
+        Hello HBNB!
+"""
 
 
 from flask import Flask
@@ -6,9 +11,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     return 'Hello HBNB!'
 
+
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
