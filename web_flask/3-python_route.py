ff850b8a457182cdd2b46e4d1e6c7da7390dee5f
Damian <damiancaetano25@gmail.com>
2022-11-29 23:34:16 +0000

    Add task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" A script that starts Flask web application
+
+    Return:
+        Hello HBNB!
+        HBNB
+        C is cool
+        Python is cool
+"""
+
+
+from flask import Flask
+from flask import escape
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_cool(text):
+    return 'C {}'.format(escape(text).replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text='is cool'):
+    return 'Python {}'.format(escape(text).replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
