435dfa11ebc1ad475b0b9ec99915098733548a81
Damian <damiancaetano25@gmail.com>
2022-11-29 18:43:10 +0000

    Add task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" A script that starts Flask web application
+
+    Return:
+        Hello HBNB!
+"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('//hbnb', strict_slashes=False)
+def HBNB():
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
