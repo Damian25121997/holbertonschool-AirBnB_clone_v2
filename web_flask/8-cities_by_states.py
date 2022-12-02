c723c2af5ebb094932f04c600122d1da88a0bb4b
Damian <damiancaetano25@gmail.com>
2022-12-02 14:00:00 +0000

    fix task 8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -17,10 +17,14 @@ def context(exception):
     storage.close()
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
+@app.route('/states_list', strict_slashes=False)
+def states_r():
+    states = storage.all(State)
+    all_states = []
+
+    for state in states.values():
+        all_states.append([state.id, state.name])
+    return render_template('7-states_list.html', states=all_states)
 
 
 @app.route('/cities_by_state', strict_slashes=False)
