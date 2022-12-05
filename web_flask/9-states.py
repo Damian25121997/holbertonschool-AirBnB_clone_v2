a6da3f0d0906dc10a82d7b5390d021b42502dea5
Damian <damiancaetano25@gmail.com>
2022-12-05 02:02:57 +0000

    fix task 10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -3,19 +3,11 @@
 Script that starts a web application
 """
 
-
 from models import storage
 from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
-
-
-@app.teardown_appcontext
-def teardown(self):
-    """ close files storage
-    """
-    storage.close()
 
 
 @app.route("/states", strict_slashes=False)
@@ -26,11 +18,18 @@ def states():
 
 @app.route("/states/<id>", strict_slashes=False)
 def states_id(id):
-    """Displays an HTML page with info about <id>, if it exists."""
     for state in storage.all("State").values():
         if state.id == id:
             return render_template("9-states.html", state=state)
     return render_template("9-states.html")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+@app.teardown_appcontext
+def teardown(exc):
+    """ close files storage
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
