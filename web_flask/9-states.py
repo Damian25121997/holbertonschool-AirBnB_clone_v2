454a6068e6540e448270e60d8cbe817bf3419fcc
Damian <damiancaetano25@gmail.com>
2022-12-05 01:49:12 +0000

    fix task 10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -4,40 +4,33 @@ Script that starts a web application
 """
 
 
-from flask import Flask, render_template
-
+from models import storage
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
 def teardown(self):
-    from models import storage
     """ close files storage
     """
     storage.close()
 
 
-@app.route('/states', strict_slashes=False)
+@app.route("/states", strict_slashes=False)
 def states():
-    from models import storage
-    from models.state import State
-    """ display states in html page
-    """
-    context = storage.all("State")
-    return render_template('9-states.html', states=context)
+    states = storage.all("State")
+    return render_template("9-states.html", state=states)
 
 
-@app.route('/states/<id>', strict_slashes=False)
+@app.route("/states/<id>", strict_slashes=False)
 def states_id(id):
-    from models import storage
-    from models.state import State
-    """ display state in html page
-    """
-    for state in storage.all('State').values():
+    """Displays an HTML page with info about <id>, if it exists."""
+    for state in storage.all("State").values():
         if state.id == id:
-            return render_template('9-states.html', state=state)
-    return render_template('9-states.html')
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
